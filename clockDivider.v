169adc805e4572ea8f5ed0a1933a8cb83e0ad16a
maz0uz <124688655+maz0uz@users.noreply.github.com>
2024-12-08 19:28:00 +0200

    Added comments

--- clockDivider.v
+++ clockDivider.v
@@ -20,17 +20,14 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module clockDivider #(parameter n = 50000000)(input clk, reset, enable, output reg clk_out);
+module clockDivider #(parameter n = 50000000)(input clk, reset, enable, output reg clk_out); // A standard parameterized clock divider module.
 wire [31:0] count;
-// Big enough to hold the maximum possible value
-// Increment count
-counter_x_bit #(32, n) counterMod (.clk(clk), .reset(reset), .enable(1'b1), .count(count));
-// Handle the output clock
+    counter_x_bit #(32, n) counterMod (.clk(clk), .reset(reset), .enable(1'b1), .count(count)); // A Counter which is used to signify when n clock cycles have been completed.
 always @ (posedge clk, posedge reset) begin
-    if (reset) // Asynchronous Reset
+    if (reset)
         clk_out <= 0;
     else if (count == n-1)
-        clk_out <= ~ clk_out;
+        clk_out <= ~ clk_out; // The clock signal is inverted whenever the counter has reached the limit.
 end
 always @(posedge clk or posedge reset) begin
     if (reset)
