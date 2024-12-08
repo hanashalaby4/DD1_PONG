ce4b8f701225e48eb8b42d7f8fd6dfc6d0675aae
maz0uz <124688655+maz0uz@users.noreply.github.com>
2024-12-08 19:29:26 +0200

    Added comments

--- counter_x_bit.v
+++ counter_x_bit.v
@@ -20,14 +20,14 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module counter_x_bit #(parameter x = 9, n = 480)(input clk, reset, enable, updown, output reg [x-1:0] count);
-    always @(posedge clk or posedge reset) begin
+module counter_x_bit #(parameter x = 9, n = 480)(input clk, reset, enable, updown, output reg [x-1:0] count); // A standard counter module which takes the number of bits (x) and the max value (n) as parameters
+    always @(posedge clk or posedge reset) begin // The count is only updated at the positive edge of the input clock.
         if (reset)
             count <= n/2;
         else if (enable) begin
             if (count == n-1)
                 count <= 0;
-            else if(updown)
+            else if(updown) // It increments when updown has value 1, and decrements when updown has value 0
                 count <= count + 1;
             else
                 count <= count - 1;
